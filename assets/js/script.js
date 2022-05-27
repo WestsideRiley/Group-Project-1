4013988ec7820628c3fb717ab01173f8d743dd83
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-05-26 19:34:35 -0500

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,40 @@
 const startBtn = document.getElementById('startBtn')
 const instructions = document.getElementById('instructions')
 const foodQuiz = document.getElementById('foodQuiz')
+const foodQuestion = document.getElementById('question')
 
 startBtn.addEventListener('click', showQuestions)
 
-function showQuestions(question, foodQuiz){
+function showQuestions(foodQuestion, foodQuiz){
     // store the answer choice 
     var output = [];
     var answers;
-    for(var i=0; i<question.length; i++){
+    for(var i=0; i<foodQuestion.length; i++){
         //reset the answer list 
         answers = [];
         //for each available answer...
-        for(letter in question[i].answers){
+        for(letter in foodQuestion[i].answers){
             //add a radio button
             answers.push(
                 '<label>'
                     + '<input type="radio" name="question'+i+'" value="'+letter+'">'
                     + letter + ': '
-                    + question[i].answers[letter]
+                    + foodQuestion[i].answers[letter]
                  + '<label>'
             );
         }
         // add this question and its answers to the output 
         output.push(
-            '<div class="question">' +question[i].question + '</div'>
+            '<div class="question">' +foodQuestion[i].foodQuestion + '</div'>
             + '<div class="answers">' + answers.join('') + '</div>'
          );
     }
     // finally combin our output list into one string of HTML and put it on the page 
     quizContainer.innerHTML = output.join('');
-};
-//show the questions
-showQuestions(question, foodQuiz);
-
-let question = {
-    question: "What is your favorite food?",
-    answers: {
-        a: Bacon & Eggs,
-        b: Spaghetti,
-        c: Tacos,
-        d: Pizza,
-        e: Steak
-    }
 }
+
+//show the questions
+showQuestions(foodQuestion, foodQuiz);
+
+
 
